1500ce0f4db89e8a80e8b6029c1506e440ee1ec6
Domingos Alves <domingos.alves@b2card.com.br>
2022-07-05 09:52:32 -0300

    Integração de duas tabelas.

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,16 @@
     <b-table :items="items" :fields="fields" striped responsive="sm">
       <template #cell(show_details)="row">
         <b-button size="sm" @click="row.toggleDetails" class="mr-2">
-          {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
+          {{ row.detailsShowing ? "Ocultar" : "Expandir" }} 
         </b-button>
-
-        <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
-        <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
-          Details via check
-        </b-form-checkbox>
       </template>
 
       <template #row-details="row">
         <b-card>
-          <b-row class="mb-2">
-            <b-col sm="3" class="text-sm-right"><b>Age:</b></b-col>
-            <b-col>{{ row.item.age }}</b-col>
-          </b-row>
 
-          <b-row class="mb-2">
-            <b-col sm="3" class="text-sm-right"><b>Is Active:</b></b-col>
-            <b-col>{{ row.item.isActive }}</b-col>
-          </b-row>
+          <b-table striped hover :items="itemsanimal" :fields="fields_teste"></b-table>
 
-          <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
+          <b-button size="sm" @click="row.toggleDetails">Ocultar</b-button>
         </b-card>
       </template>
     </b-table>
@@ -35,25 +23,41 @@
 //import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
-      return {
-        fields: ['first_name', 'last_name', 'show_details'],
-        items: [
-          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          {
-            isActive: false,
-            age: 89,
-            first_name: 'Geneva',
-            last_name: 'Wilson',
-            _showDetails: true
-          },
-          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
-      }
-    }
-}
+    return {
+      fields: ["primeiro_nome", "sobrenome", "show_details"],
+      items: [
+        {
+          isActive: true,
+          idade: 40,
+          primeiro_nome: "Dickerson",
+          sobrenome: "Macdonald",
+        },
+        { isActive: false, idade: 21, primeiro_nome: "Larsen", sobrenome: "Shaw" },
+        {
+          isActive: false,
+          idade: 89,
+          primeiro_nome: "Geneva",
+          sobrenome: "Wilson",
+          _showDetails: false,
+        },
+        { isActive: true, idade: 38, primeiro_nome: "Jami", sobrenome: "Carney" },
+      ],
+      fields_teste: ["nome_animal", "sexo"],
+      itemsanimal: [
+        {
+          nome_animal: "Gabiru",
+          sexo: "Macho",
+        },
+        {
+          nome_animal: "Belinha",
+          sexo: "Fêmea",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
