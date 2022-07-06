9b894c180fa86fcd60d7de989f1eebf9f21466f7
Domingos Alves <73261252+domrms@users.noreply.github.com>
2022-07-06 19:49:49 -0300

    Organização do código.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="app">
-    <b-table :items="items" :fields="fields" striped responsive="sm">
+    <b-table :items="items_cooperativa" :fields="fields_cooperativa" striped responsive="sm">
       <template #cell(expandir_cooperativa)="row">
         <b-button size="sm" @click="row.toggleDetails" class="cooperativa">
           {{ row.detailsShowing ? "Ocultar" : "Expandir" }}
         </b-button>
       </template>
+      
       <template #row-details="row">
         <b-card>
-          <b-table :items="itemsanimal" :fields="fields_teste">
-            <template #cell(expandir_filho)="row">
-              <b-button size="sm" @click="row.toggleDetails" class="filho">
+          <b-table :items="items_agencia" :fields="fields_agencia">
+            <template #cell(expandir_agencia)="row">
+              <b-button size="sm" @click="row.toggleDetails" class="agencia">
                 {{ row.detailsShowing ? "Ocultar" : "Expandir" }}
               </b-button>
             </template>
 
-            <template #row-details="row">
+            <template #row-details="">
               <b-card>
-                <b-table :items="itemsanimal2" :fields="fields_teste2">
-                  <template #cell(expandir_neto)="row">
+                <b-table :items="items_tipoDeTransacao" :fields="fields_tipoDeTransacao">
+                  <template #cell(expandir_tipoDeTransacao)="row">
                     <b-button
                       size="sm"
                       @click="row.toggleDetails"
@@ -27,21 +28,7 @@
                       {{ row.detailsShowing ? "Ocultar" : "Expandir" }}
                     </b-button>
                   </template>
-                  <template #row-details="row">
-                    <b-card>
-                      <b-table :items="itemsanimal3" :fields="fields_teste3">
-                        <template #cell(expandir_neto)="row">
-                          <b-button
-                            size="sm"
-                            @click="row.toggleDetails"
-                            class="filho"
-                          >
-                            {{ row.detailsShowing ? "Ocultar" : "Expandir" }}
-                          </b-button>
-                        </template>
-                      </b-table>
-                    </b-card>
-                  </template>
+                  
                 </b-table>
               </b-card>
             </template>
@@ -58,12 +45,41 @@ export default {
   name: "App",
   data() {
     return {
-      fields: ["cooperativa", "agencia", "nomeDaCooperativa", "expandir_cooperativa"],
-      items: [
+      fields_cooperativa: [
+        "cooperativa",
+        "nomeDaCooperativa",
+        "expandir_cooperativa",
+      ],
+      items_cooperativa: [
         {
           cooperativa: "0555",
+          nomeDaCooperativa: "Unicred",
+        },
+      ],
+      fields_agencia: [
+        "agencia",
+        "nomeDaAgencia",
+        "cidadeDaAgencia",
+        "expandir_agencia"
+      ],
+      items_agencia: [
+        {
           agencia: "teste1",
-          nomeDaCooperativa: "Unicred"
+          nomeDaAgencia: "Banco do Brasil 222",
+          cidadeDaAgencia: "Goiânia - GO",
+        },
+      ],
+      fields_tipoDeTransacao: [
+        "visaCredito",
+        "visaDebito",
+        "dataDoClearing",
+        //"expandir_tipoDeTransacao"
+      ],
+      items_tipoDeTransacao: [
+        {
+          visaCredito: "true",
+          visaDebito: "false",
+          dataDoClearing: "05/07/2022",
         },
       ],
     };
@@ -72,6 +88,12 @@ export default {
 </script>
 
 <style>
-
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
