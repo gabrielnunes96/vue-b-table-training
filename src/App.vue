add9a27a7bcf5b6ab648013a14a2a246e8455365
Domingos Alves <73261252+domrms@users.noreply.github.com>
2022-07-06 19:17:50 -0300

    Funcionamento correto do código.

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,30 @@
 <template>
   <div id="app">
     <b-table :items="items" :fields="fields" striped responsive="sm">
-      <template #cell(expandir_pai)="row">
-        <b-form-checkbox
-          v-model="row.detailsShowing"
-          @change="row.toggleDetails"
-        >
+      <template #cell(expandir_cooperativa)="row">
+        <b-button size="sm" @click="row.toggleDetails" class="cooperativa">
           {{ row.detailsShowing ? "Ocultar" : "Expandir" }}
-<<<<<<< HEAD
         </b-button>
-=======
-        </b-form-checkbox>
->>>>>>> origin/lucas-dev
       </template>
-      <template #row-details="">
-        <b-table :items="itemsanimal" :fields="fields_teste">
-          <template #cell(expandir_filho)="row">
-            <b-form-checkbox
-              v-model="row.detailsShowing"
-              @change="row.toggleDetails"
-            >
-              {{ row.detailsShowing ? "Ocultar" : "Expandir" }}
-            </b-form-checkbox>
-          </template>
+      <template #row-details="row">
+        <b-card>
+          <b-table :items="itemsanimal" :fields="fields_teste">
+            <template #cell(expandir_filho)="row">
+              <b-button size="sm" @click="row.toggleDetails" class="filho">
+                {{ row.detailsShowing ? "Ocultar" : "Expandir" }}
+              </b-button>
+            </template>
 
-<<<<<<< HEAD
             <template #row-details="row">
               <b-card>
                 <b-table :items="itemsanimal2" :fields="fields_teste2">
-=======
-          <template #row-details="">
-            <b-table :items="itemsanimal2" :fields="fields_teste2">
-              <template #cell(expandir_neto)="row">
-                <b-form-checkbox
-                  v-model="row.detailsShowing"
-                  @change="row.toggleDetails"
-                >
-                  {{ row.detailsShowing ? "Ocultar" : "Expandir" }}
-                </b-form-checkbox>
-              </template>
-              <template #row-details="">
-                <b-table :items="itemsanimal3" :fields="fields_teste3">
->>>>>>> origin/lucas-dev
                   <template #cell(expandir_neto)="row">
-                    <b-form-checkbox
-                      v-model="row.detailsShowing"
-                      @change="row.toggleDetails"
+                    <b-button
+                      size="sm"
+                      @click="row.toggleDetails"
+                      class="filho"
                     >
                       {{ row.detailsShowing ? "Ocultar" : "Expandir" }}
-<<<<<<< HEAD
                     </b-button>
                   </template>
                   <template #row-details="row">
@@ -65,15 +41,13 @@
                         </template>
                       </b-table>
                     </b-card>
-=======
-                    </b-form-checkbox>
->>>>>>> origin/lucas-dev
                   </template>
                 </b-table>
-              </template>
-            </b-table>
-          </template>
-        </b-table>
+              </b-card>
+            </template>
+          </b-table>
+          <b-button size="sm" @click="row.toggleDetails">Ocultar</b-button>
+        </b-card>
       </template>
     </b-table>
   </div>
@@ -84,87 +58,20 @@ export default {
   name: "App",
   data() {
     return {
-<<<<<<< HEAD
-      items: {
-        dataDaAgenda: {
-          cooperativa: {
-            1111: {
-              agencia: {
-                5565: {
-                  tipoDeTransacao: "Crédito",
-                },
-                9988: {
-                  tipoDeTransacao: "Débito",
-                },
-              },
-            },
-            2222: {
-              agencia: {
-                5151: {
-                  tipoDeTransacao: "Crédito",
-                },
-                5445: {
-                  tipoDeTransacao: "Débito",
-                },
-              },
-            },
-            3333: {
-              agencia: {
-                5151: {
-                  tipoDeTransacao: "Crédito",
-                },
-                5445: {
-                  tipoDeTransacao: "Débito",
-                },
-              },
-            },
-          },
-        },
-      },
-=======
-      fields: ["expandir_pai", "primeiro_nome", "sobrenome", "idade"],
+      fields: ["cooperativa", "agencia", "nomeDaCooperativa", "expandir_cooperativa"],
       items: [
         {
-          temGato: false,
-          idade: 21,
-          primeiro_nome: "Barack",
-          sobrenome: "Obama",
+          cooperativa: "0555",
+          agencia: "teste1",
+          nomeDaCooperativa: "Unicred"
         },
       ],
-      fields_teste: ["expandir_filho", "nome_animal", "sexo"],
-      itemsanimal: [
-        {
-          nome_animal: "Gabiru",
-          sexo: "Macho",
-        },
-      ],
-      fields_teste2: ["expandir_neto", "cor_pelo", "brinquedo"],
-      itemsanimal2: [
-        {
-          cor_pelo: "Preto",
-          brinquedo: "Bola",
-        },
-      ],
-      fields_teste3: ["cheiro_coco", "cor_caixa_areia"],
-      itemsanimal3: [
-        {
-          cheiro_coco: "Fedido",
-          cor_caixa_areia: "Azul",
-        },
-      ],
->>>>>>> origin/lucas-dev
     };
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
